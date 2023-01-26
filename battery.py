5e7d6a3b686fff7eaa58a6d20062091f7df0090a
pablo-alcaniz <93190637+pablo-alcaniz@users.noreply.github.com>
2023-01-26 20:07:39 +0100

    Update battery.py

--- battery.py
+++ battery.py
@@ -2,6 +2,8 @@ import time
 import subprocess
 
 time_sleep = 0.5
+cont = True
+last_status = 1
 
 g = open('/sys/class/power_supply/ADP1/online')
 status_ini = int(g.read(1))
@@ -10,8 +12,9 @@ if status_ini == 1:
     subprocess.call("/bin/battery_module/log_ac.sh")
     subprocess.call("/bin/battery_module/backlight_ac.sh")
     subprocess.call("/bin/battery_module/epb_ac.sh")
-    subprocess.call("/bin/battery_module/governor_ac.sh")
+    subprocess.call("/bin/battery_module/governor_batt.sh")
     subprocess.call("/bin/battery_module/turbo_boost_ac.sh")
+    subprocess.call("/bin/battery_module/freq_ac.sh")
     last_status = 1
 elif status_ini == 0:
     subprocess.call("/bin/battery_module/log_batt.sh")
@@ -19,6 +22,7 @@ elif status_ini == 0:
     subprocess.call("/bin/battery_module/epb_batt.sh")
     subprocess.call("/bin/battery_module/governor_batt.sh")
     subprocess.call("/bin/battery_module/turbo_boost_batt.sh")
+    subprocess.call("/bin/battery_module/freq_batt.sh")
     last_status = 0
 
 while True:
@@ -33,8 +37,9 @@ while True:
             subprocess.call("/bin/battery_module/log_ac.sh")
             subprocess.call("/bin/battery_module/backlight_ac.sh")
             subprocess.call("/bin/battery_module/epb_ac.sh")
-            subprocess.call("/bin/battery_module/governor_ac.sh")
+            subprocess.call("/bin/battery_module/governor_batt.sh")
             subprocess.call("/bin/battery_module/turbo_boost_ac.sh")
+            subprocess.call("/bin/battery_module/freq_ac.sh")
             last_status = 1
             time.sleep(time_sleep)
         elif status == 0:
@@ -43,6 +48,7 @@ while True:
             subprocess.call("/bin/battery_module/epb_batt.sh")
             subprocess.call("/bin/battery_module/governor_batt.sh")
             subprocess.call("/bin/battery_module/turbo_boost_batt.sh")
+            subprocess.call("/bin/battery_module/freq_batt.sh")
             last_status = 0
             time.sleep(time_sleep)
         else:
